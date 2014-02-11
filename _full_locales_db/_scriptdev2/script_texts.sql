1af5420e0e80ffee244cdc8f95ae5c1bcea1c5d9
Salja <salja8840@gmail.com>
2014-02-11 22:24:04 +0100

    Import locales for scriptdev2 "ONLY" Sunwell DONE!
    Signed-off-by: Salja <salja8840@gmail.com>

---
+++ _full_locales_db/_scriptdev2/script_texts.sql
@@ -0,0 +1,142 @@
+﻿--
+-- Copyright (C) 2005-2014 MaNGOS <http://getmangos.com/>
+--
+-- This program is free software; you can redistribute it and/or modify
+-- it under the terms of the GNU General Public License as published by
+-- the Free Software Foundation; either version 2 of the License, or
+-- (at your option) any later version.
+--
+-- This program is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU General Public License for more details.
+--
+-- You should have received a copy of the GNU General Public License
+-- along with this program; if not, write to the Free Software
+-- Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+--
+
+#####	script_texts	#####
+
+SET NAMES 'utf8';
+UPDATE script_texts SET content_loc3=NULL;
+
+-- -1 580 000 SUNWELL PLATEAU
+UPDATE script_texts SET content_loc3='Ah ha haa!! Nicht länger werde ich Malygos\' Sklave sein! Fordert mich heraus und Ihr werdet vernichtet!' WHERE entry=-1580000;
+UPDATE script_texts SET content_loc3='Ich werde Euch auslöschen!' WHERE entry=-1580001;
+UPDATE script_texts SET content_loc3='Dies ist nur der Anfang Eurer Qualen!' WHERE entry=-1580002;
+UPDATE script_texts SET content_loc3='Im Namen Kil\'jaedens!' WHERE entry=-1580003;
+UPDATE script_texts SET content_loc3='Ihr wurdet gewarnt!' WHERE entry=-1580004;
+UPDATE script_texts SET content_loc3='Es ist vollbracht! Ich bin erwacht! Sterben sollt ihr alle!' WHERE entry=-1580005;
+UPDATE script_texts SET content_loc3='Helft mir... kann... ihm... nicht länger... widerstehen...' WHERE entry=-1580006;
+UPDATE script_texts SET content_loc3='Aaahhh! Helft mir, bevor ich den Verstand verliere!' WHERE entry=-1580007;
+UPDATE script_texts SET content_loc3='Beeilt euch! Es ist nicht mehr viel von mir da!' WHERE entry=-1580008;
+UPDATE script_texts SET content_loc3='Ich stehe für immer in eurer Schuld. Sobald wir über Kil\'jaeden triumphiert haben, wird diese gesamte Welt in eurer Schuld stehen.' WHERE entry=-1580009;
+UPDATE script_texts SET content_loc3='Uhahaha... Niemand wird verschont werden. Meine Arbeit hier ist fast vollbracht.' WHERE entry=-1580010;
+UPDATE script_texts SET content_loc3='Ich... bin niemals auf der... Verlierer... seite!' WHERE entry=-1580011;
+UPDATE script_texts SET content_loc3='Ich schwelge in eurem Elend!' WHERE entry=-1580012;
+UPDATE script_texts SET content_loc3='Ich werde euch beim Bluten zusehen!' WHERE entry=-1580013;
+UPDATE script_texts SET content_loc3='Erbärmliche Sterbliche!' WHERE entry=-1580014;
+UPDATE script_texts SET content_loc3='Habt ihr es noch nicht gehört? Ich gewinne immer!' WHERE entry=-1580015;
+UPDATE script_texts SET content_loc3='Ich habe lange genug mit Euch herumgespielt!' WHERE entry=-1580016;
+
+UPDATE script_texts SET content_loc3='Mickrige Echse! Tod ist die einzige Antwort die du hier finden wirst!' WHERE entry=-1580017;
+UPDATE script_texts SET content_loc3='Bah! Deine schäbige Magie ist schwach!' WHERE entry=-1580018;
+UPDATE script_texts SET content_loc3='Ich werde dich zerquetschen!' WHERE entry=-1580019;
+UPDATE script_texts SET content_loc3='Das hat Spaß gemacht.' WHERE entry=-1580020;
+UPDATE script_texts SET content_loc3='Kommt, und versucht euer Glück!' WHERE entry=-1580021;
+UPDATE script_texts SET content_loc3='Ahh! Mehr Lämmer zum Schlachten!' WHERE entry=-1580022;
+UPDATE script_texts SET content_loc3='Stirb, Insekt!' WHERE entry=-1580023;
+UPDATE script_texts SET content_loc3='Du bist Hackfleisch!' WHERE entry=-1580024;
+UPDATE script_texts SET content_loc3='Zu einfach!' WHERE entry=-1580025;
+UPDATE script_texts SET content_loc3='Bringt den Kampf zu mir!' WHERE entry=-1580026;
+UPDATE script_texts SET content_loc3='Ein neuer Tag, eine neue glorreiche Schlacht!' WHERE entry=-1580027;
+UPDATE script_texts SET content_loc3='Dafür lebe ich!' WHERE entry=-1580028;
+UPDATE script_texts SET content_loc3='Soviel zur echten Herausforderung... Sterbt!' WHERE entry=-1580029;
+UPDATE script_texts SET content_loc3='Uhh! Gut gemacht... Nun... wird es langsam... interessant...' WHERE entry=-1580030;
+
+UPDATE script_texts SET content_loc3='Haltet ein, Freunde! Es gibt hier noch etwas zu erfahren bevor dieser Teufel seinem Schicksal ins Auge blickt!' WHERE entry=-1580031;
+UPDATE script_texts SET content_loc3='Wo ist Anveena, Dämon? Was ist mit Kalec geschehen?' WHERE entry=-1580032;
+UPDATE script_texts SET content_loc3='Du wirst mir sagen wo sie sind!' WHERE entry=-1580033;
+UPDATE script_texts SET content_loc3='Sprich, ich bin es leid zu fragen!' WHERE entry=-1580034;
+UPDATE script_texts SET content_loc3='Malygos, mein Lord! Ich habe mein Bestes getan!' WHERE entry=-1580035;
+
+UPDATE script_texts SET content_loc3='Ehre sei Kil\'jaeden! Tod allen, die sich ihm widersetzen!' WHERE entry=-1580036;
+UPDATE script_texts SET content_loc3='Ich töte für den Meister!' WHERE entry=-1580037;
+UPDATE script_texts SET content_loc3='Das Ende ist gekommen!' WHERE entry=-1580038;
+UPDATE script_texts SET content_loc3='Ersticke an deinem letzten Atemzug!' WHERE entry=-1580039;
+UPDATE script_texts SET content_loc3='Ich bin stärker als je zuvor!' WHERE entry=-1580040;
+UPDATE script_texts SET content_loc3='Das Zögern hat ein Ende! Euer Schicksal ist besiegelt!' WHERE entry=-1580041;
+UPDATE script_texts SET content_loc3='Kil\'jaeden wird... obsiegen!' WHERE entry=-1580042;
+UPDATE script_texts SET content_loc3='Madrigosa hat ein besseres Schicksal verdient. Ihr habt getan, was getan werden musste - doch diese Schlacht ist noch lange nicht vorbei!' WHERE entry=-1580043;
+
+UPDATE script_texts SET content_loc3='Elend...' WHERE entry=-1580044;
+UPDATE script_texts SET content_loc3='Leid...' WHERE entry=-1580045;
+UPDATE script_texts SET content_loc3='Verwirrung...' WHERE entry=-1580046;
+UPDATE script_texts SET content_loc3='Hass...' WHERE entry=-1580047;
+UPDATE script_texts SET content_loc3='Misstrauen...' WHERE entry=-1580048;
+UPDATE script_texts SET content_loc3='Chaos...' WHERE entry=-1580049;
+UPDATE script_texts SET content_loc3='Dies sind die Kennzeichen...' WHERE entry=-1580050;
+UPDATE script_texts SET content_loc3='Dies sind die Säulen...' WHERE entry=-1580051;
+
+UPDATE script_texts SET content_loc3='Schatten, dem Feuer zur Hilfe!' WHERE entry=-1580052;
+UPDATE script_texts SET content_loc3='Alythess! Dein Feuer brennt in mir!' WHERE entry=-1580053;
+UPDATE script_texts SET content_loc3='Schatten verschlingt!' WHERE entry=-1580054;
+UPDATE script_texts SET content_loc3='Ee-nok Kryul!' WHERE entry=-1580055;
+UPDATE script_texts SET content_loc3='Ich... vergehe.' WHERE entry=-1580056;
+UPDATE script_texts SET content_loc3='Zeit ist ein Luxus, den Ihr nicht mehr habt!' WHERE entry=-1580057;
+UPDATE script_texts SET content_loc3='Feuer dem Schatten zu Hilfe!' WHERE entry=-1580058;
+UPDATE script_texts SET content_loc3='Sacrolash!' WHERE entry=-1580059;
+UPDATE script_texts SET content_loc3='Feuer, verzehre!' WHERE entry=-1580060;
+UPDATE script_texts SET content_loc3='Edir harach!' WHERE entry=-1580061;
+UPDATE script_texts SET content_loc3='Der\'ek... manul.' WHERE entry=-1580062;
+UPDATE script_texts SET content_loc3='Euer Glück ist verbraucht!' WHERE entry=-1580063;
+
+UPDATE script_texts SET content_loc3='Dies ist der Zweck all meiner Pläne!' WHERE entry=-1580064;
+UPDATE script_texts SET content_loc3='Bleibt bei der Sache, verliert keine Zeit.' WHERE entry=-1580065;
+UPDATE script_texts SET content_loc3='Ich habe lange genug gewartet!' WHERE entry=-1580066;
+UPDATE script_texts SET content_loc3='Enttäuscht mich und Ihr werdet in alle Ewigkeit leiden!' WHERE entry=-1580067;
+UPDATE script_texts SET content_loc3='Entzieht dem Mädchen die Kraft! Entzieht sie Ihr, bis sie nicht\'s weiter als eine leere Hülle ist!' WHERE entry=-1580068;
+UPDATE script_texts SET content_loc3='Die Entbehrlichen sind dahin - so sei es! Jetzt werde ich dort erfolgreich sein, wo Sargeras versagt hat! Ich werde diese jämmerliche Welt ausbluten lassen und meinen Platz als wahrer Meister der Brennenden Legion einnehmen! Das Ende ist gekommen! Lasst uns diese Welt dem Erdboden gleichmachen!' WHERE entry=-1580069;
+UPDATE script_texts SET content_loc3='Der Vernichtung einen weiteren Schritt näher!' WHERE entry=-1580070;
+UPDATE script_texts SET content_loc3='Anukh-Kyrie!' WHERE entry=-1580071;
+UPDATE script_texts SET content_loc3='Wem könnt Ihr vertrauen?' WHERE entry=-1580072;
+UPDATE script_texts SET content_loc3='Der Feind ist unter euch!' WHERE entry=-1580073;
+UPDATE script_texts SET content_loc3='Chaos!' WHERE entry=-1580074;
+UPDATE script_texts SET content_loc3='Zerstörung!' WHERE entry=-1580075;
+UPDATE script_texts SET content_loc3='Untergang!' WHERE entry=-1580076;
+UPDATE script_texts SET content_loc3='Nichts wird mich aufhalten! Diese Welt wird fallen!' WHERE entry=-1580077;
+UPDATE script_texts SET content_loc3='Hegt keine falschen Hoffnungen! Ihr könnt nicht gewinnen!' WHERE entry=-1580078;
+UPDATE script_texts SET content_loc3='Arrrghh! Die Energie des Sonnenbrunnens... richtet sich... gegen mich! Was habt ihr getan? Was habt ihr getan?!!' WHERE entry=-1580079;
+UPDATE script_texts SET content_loc3='Ihr seid nicht allein! Der blaue Drachenschwarm wird Euch dabei helfen, den Betrüger zu bezwingen!' WHERE entry=-1580080;
+UPDATE script_texts SET content_loc3='Erwache, Anveena! Diese Welt braucht dich!' WHERE entry=-1580081;
+UPDATE script_texts SET content_loc3='Ich diene jetzt nur noch dem Meister!' WHERE entry=-1580082;
+UPDATE script_texts SET content_loc3='Du musst loslassen! Du musst das werden, wozu du schon immer bestimmt warst! Die Zeit ist reif, Anveena!' WHERE entry=-1580083;
+UPDATE script_texts SET content_loc3='Ich bin... verloren. Ich finde nicht mehr zurück.' WHERE entry=-1580084;
+UPDATE script_texts SET content_loc3='Anveena, ich liebe dich! Höre auf meine Worte! Komm zu mir zurück! Nur du kannst den Sonnenbrunnen läutern!' WHERE entry=-1580085;
+UPDATE script_texts SET content_loc3='Kalec... Kalec?' WHERE entry=-1580086;
+UPDATE script_texts SET content_loc3='Ja, Anveena! Gib dich deinem Schicksal hin!' WHERE entry=-1580087;
+UPDATE script_texts SET content_loc3='Der Alptraum ist vorbei! Der Zauber ist gebrochen! Adieu Kalec, mein Liebster!' WHERE entry=-1580088;
+UPDATE script_texts SET content_loc3='Adieu, Anveena, meine Liebe. Nur wenige werden sich an deinen Namen erinnern, doch heute hast du den Lauf des Schicksals verändert. Was einst verdorben war, ist nun rein. Helden, ihr Opfer darf nicht umsonst gewesen sein.' WHERE entry=-1580089;
+UPDATE script_texts SET content_loc3='Nun schlagt zu, Helden, solange er geschwächte ist! Bezwingt den Betrüger!' WHERE entry=-1580090;
+UPDATE script_texts SET content_loc3='Ich werde die Kugeln mit meiner Macht erfüllen! Seid bereit!' WHERE entry=-1580091;
+UPDATE script_texts SET content_loc3='Eine weitere Kugel ist von meiner Macht erfüllt! Benutzt sie, schnell!' WHERE entry=-1580092;
+UPDATE script_texts SET content_loc3='Eine weitere Kugel ist bereit! Sputet Euch!' WHERE entry=-1580093;
+UPDATE script_texts SET content_loc3='Ich habe getan, was ich konnte! Die Macht liegt in Euren Händen!' WHERE entry=-1580094;
+
+UPDATE script_texts SET content_loc3='Sterbliche. Euer Sieg wurde schon vor langer Zeit vorher gesagt. Der qualvolle Schrei der Niederlage meines Bruder wird durch das Universum schallen und all jenen Hoffnung spenden, die sich der Legion entgegen stellen.' WHERE entry=-1580095;
+UPDATE script_texts SET content_loc3='Stetig rückt die entgültige Niederlage der Legion näher. So wisset und seid stolz das Ihr zahllose Welten vor der Flamme bewahrt habt.' WHERE entry=-1580096;
+UPDATE script_texts SET content_loc3='So wie dieser Tag ein Ende ist, so ist er auch ein neuer Anfang!' WHERE entry=-1580097;
+UPDATE script_texts SET content_loc3='Entropius, das Wesen, welches Ihr zerstören musstet, war der Naruu, M\'uru. Der einst unermäßliche Energien des Lichts und der Hoffnung spenndete. Es gab eine Zeit, da versuchten Fehlgeleitete diese Energie zu stehlen!' WHERE entry=-1580098;
+UPDATE script_texts SET content_loc3='Unsere Arroganz ist unverzeihlich. Verdammnis brachten wir über eines der edelsten Wesen. Eine Schuld die ewig währt!' WHERE entry=-1580099;
+UPDATE script_texts SET content_loc3='Ein Glück das ich den Funken des noblen Naruu von dort geborgen habe, wo er fiel. Wo es Glauben gibt ist die Hoffnung niemals verloren, junge Blutelfe.' WHERE entry=-1580100;
+UPDATE script_texts SET content_loc3='Kann es sein?' WHERE entry=-1580101;
+UPDATE script_texts SET content_loc3='Sehet nun, Sterbliche. Erblicket M\'uru\'s Herz! Markellos. Getränkt im Lichte der Schöpfung. So wie es von Anbeginn an war.' WHERE entry=-1580102;
+UPDATE script_texts SET content_loc3='Sein Licht und seine Hoffnung werden mehr als nur diesen Quell der Macht zu neuem Leben erwecken. Vielleicht gar die Seele eines Volkes.' WHERE entry=-1580103;
+UPDATE script_texts SET content_loc3='Gesegnete Vorfahren. Ich fühle es... soviel Liebe... so viel Gnade... keine Worte... unmöglich zu beschreiben.' WHERE entry=-1580104;
+UPDATE script_texts SET content_loc3='Erlösung, mein Kind. Sie erwartet uns alle.' WHERE entry=-1580105;
+UPDATE script_texts SET content_loc3='Lebt wohl...!' WHERE entry=-1580106;
+
+UPDATE script_texts SET content_loc3='%s holt tief Luf.' WHERE entry=-1580107;
+
+SET NAMES 'latin1';
